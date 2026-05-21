79d5ea98c3af0e31ab70f8a2984b9ea87e9c88c6
shykat-tech <shykat@co.design>
2026-05-21 15:51:02 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 App deployed via CI/CD -- Server");
+  res.send("🚀 App deployed via CI/CD | Server");
 });
 
 app.listen(3000, () => {
